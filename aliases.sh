379a016983ba42301dade16e9b8fab59d6190d25
Adam Lugowski <alugowski@gmail.com>
2018-07-11 21:24:02 -0700

    Add clear alias

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,7 @@
 set +H  # disable history lookup so the !echo below doesn't get misinterpreted
 alias detect_encoding="vim -c 'execute \"silent !echo \" . &fileencoding | q'"
 
+# clear that clears iTerm2's scrollback too
+# see https://superuser.com/a/726295/129020
+alias clear="printf '\e]50;ClearScrollback\a'"
+
