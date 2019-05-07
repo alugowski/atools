1af7262230ac619f24b108d6046f1f60f64fce0f
Adam Lugowski <alugowski@gmail.com>
2019-05-07 16:56:44 -0700

    Dont write .DS_Store files on remote shares

--- once-mac.sh
+++ once-mac.sh
@@ -5,3 +5,6 @@
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 2
 
+# Don't write .DS_Store files on network mounts
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
