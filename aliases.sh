46a1fd5168d7fe92878699f7973b8b0c165aed7a
Adam Lugowski <alugowski@gmail.com>
2019-09-03 19:09:26 -0700

    Add unlock alias

--- aliases.sh
+++ aliases.sh
@@ -11,3 +11,7 @@ alias clear="printf '\e]50;ClearScrollback\a'"
 # Mount a phone using simple-mtpfs
 # install using `brew install simple-mtpfs`. Might prompt an installation of OSXFuse.
 alias mount_phone_fg="simple-mtpfs -f ~/phone"
+
+# Remove the locked flag
+# This flag is set for files that were locked in-camera
+alias unlock="chflags -R nouchg"
