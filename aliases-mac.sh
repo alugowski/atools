d1c393cead56fc9235b9b8308ba88ec2fb60c531
Adam Lugowski <alugowski@gmail.com>
2021-01-19 12:14:01 -0600

    Move some mac-only aliases to aliases-mac

--- aliases-mac.sh
+++ aliases-mac.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Mount a phone using simple-mtpfs
+# install using `brew install simple-mtpfs`. Might prompt an installation of OSXFuse.
+alias mount_phone_fg="simple-mtpfs -f ~/phone"
+
+# Remove the locked flag
+# This flag is set for files that were locked in-camera
+alias unlock="chflags -R nouchg"
+
 # Nassie access
 nassie-smb-over-ssh () {
   $ATOOLS_SCRIPTS_HOME/scripts/smb-over-ssh/smb-over-ssh.sh nassie nlugowski
--- aliases.sh
+++ aliases.sh
@@ -8,14 +8,6 @@ alias detect_encoding="vim -c 'execute \"silent !echo \" . &fileencoding | q'"
 # see https://superuser.com/a/726295/129020
 alias clear="printf '\e]50;ClearScrollback\a'"
 
-# Mount a phone using simple-mtpfs
-# install using `brew install simple-mtpfs`. Might prompt an installation of OSXFuse.
-alias mount_phone_fg="simple-mtpfs -f ~/phone"
-
-# Remove the locked flag
-# This flag is set for files that were locked in-camera
-alias unlock="chflags -R nouchg"
-
 # Create and activate a new Python virtualenv, or activate an already existing one
 venv () {
 	if [ ! -d env/ ]; then
