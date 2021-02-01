989beffffd6ead8d898ef0248a133b6e13b02466
Adam Lugowski <alugowski@gmail.com>
2021-02-01 17:59:02 -0600

    Add la and psgrep aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+alias la="ls -la"
+alias psgrep="ps -A | grep"
+
 # Wrap VIM's text encoding detector
 set +H  # disable history lookup so the !echo below doesn't get misinterpreted
 alias detect_encoding="vim -c 'execute \"silent !echo \" . &fileencoding | q'"
