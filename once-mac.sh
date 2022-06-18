153c911e74c3ff9cc9a7ba3fc01be58cb5f82b3d
Adam Lugowski <alugowski@gmail.com>
2022-06-17 17:33:25 -0700

    Add Mac Finder default list view

--- once-mac.sh
+++ once-mac.sh
@@ -8,3 +8,7 @@ defaults write -g KeyRepeat -int 2
 # Don't write .DS_Store files on network mounts
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
