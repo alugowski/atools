24a1e1903934a653b849c893b42fc40bbe6688c2
Adam Lugowski <alugowski@gmail.com>
2023-09-13 12:08:49 -0700

    Add condashellinit

--- aliases-mac.sh
+++ aliases-mac.sh
@@ -20,3 +20,24 @@ kill-smb-over-ssh-tunnels () {
 # Restart Logitech Options. Useful if the Logitech mouse scroll wheel stops working.
 alias logirestart="kill $(ps aux | grep LogiMgrDaemon.app | awk 'FNR == 1 {print $2}')"
 
+# x86 Homebrew
+# installed with: arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+alias ibrew="arch -x86_64 /usr/local/bin/brew"
+
+
+condashellinit () {
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+}
